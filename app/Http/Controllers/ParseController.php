1c7b9ba14925d62bde372ff6f4824a692deef1f4
Levan Velijanashvili <stichoza@gmail.com>
2015-11-12 17:13:09 +0400

    Empty spaces...
    What are we liiiiiving foooor?

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -8,47 +8,51 @@ use Illuminate\Http\Request;
 class ParseController extends Controller
 {
 
-	/**
-	 * PdfParser object
-	 * @var \Smalot\PdfParser\Parser
-	 */
-	private $parser;
+    /**
+     * PdfParser object
+     * @var \Smalot\PdfParser\Parser
+     */
+    private $parser;
 
     public function dump(Request $request)
     {
-    	$this->parser = new PdfParser();
-		$pdf = $this->parser->parseFile('https://www.tsu.ge/data/file_db/sagamocdo/12.11.15_13.00.pdf');
-		 
-		$text = $pdf->getText();
-		
-		$data = [];
-		preg_match_all('/^([ა-ჰ]+)([\d]{2,4})(.+)$/im', $text, $data);
 
-		$search = $request->input('q');
+        $date = $request->input('date');
+        $date = $request->input('time');
 
-		$results = [];
+        $this->parser = new PdfParser();
+        $pdf = $this->parser->parseFile('https://www.tsu.ge/data/file_db/sagamocdo/12.11.15_13.00.pdf');
+         
+        $text = $pdf->getText();
+        
+        $data = [];
+        preg_match_all('/^([ა-ჰ]+)([\d]{2,4})(.+)$/im', $text, $data);
 
-		for ($i=0; $i < count($data[0]); $i++) { 
+        $search = $request->input('q');
 
-			if ($data[1][$i] == $search) {
-				$temp = ['subject' => $data[3][$i]];
+        $results = [];
 
-				if (strlen($data[2][$i]) == 2) {
-					$temp['sector'] = substr($data[2][$i], 0, 1);
-					$temp['seat']   = substr($data[2][$i], -1);
-				} else if (strlen($data[2][$i]) == 4) {
-					$temp['sector'] = substr($data[2][$i], 0, 2);
-					$temp['seat']   = substr($data[2][$i], -2);
-				} else {
-					// wtf
-				}
+        for ($i=0; $i < count($data[0]); $i++) { 
 
-				$results[] = $temp;
-				
-			}
-		}
+            if ($data[1][$i] == $search) {
+                $temp = ['subject' => $data[3][$i]];
+
+                if (strlen($data[2][$i]) == 2) {
+                    $temp['sector'] = substr($data[2][$i], 0, 1);
+                    $temp['seat']   = substr($data[2][$i], -1);
+                } else if (strlen($data[2][$i]) == 4) {
+                    $temp['sector'] = substr($data[2][$i], 0, 2);
+                    $temp['seat']   = substr($data[2][$i], -2);
+                } else {
+                    // wtf
+                }
+
+                $results[] = $temp;
+                
+            }
+        }
 
 
-		dd($results);
+        dd($results);
     }
 }

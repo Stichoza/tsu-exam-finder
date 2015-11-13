9b2d19140cd319c7046dd2f444813176810246b0
Levan Velijanashvili <stichoza@gmail.com>
2015-11-13 07:07:56 +0400

    This is epic! 🍻

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,14 @@
 	<style>
 		body {
 			background-color: #5ca0a0;
+			font-family: 'ALK Life';
 		}
 		.svg-desk {
 			position: fixed;
 			top: 0;
 			left: 0;
 			right: 0;
-			bottom: 30%;
+			bottom: 24%;
 			background-image: url('assets/desk.svg');
 			background-size: cover;
 			z-index: 200;
@@ -34,20 +35,20 @@
 			margin-bottom: 0;
 		}
 		.svg-shelf-r {
-			bottom: calc(30% - 52px);
+			bottom: calc(24% - 52px);
 			margin-left: 240px;
 			margin-bottom: 20px;
 			width: 140px;
 		}
 		.svg-shelf-l {
-			bottom: calc(30% - 84px);
+			bottom: calc(24% - 84px);
 			margin-left: -350px;
 			margin-bottom: 50px;
 			width: 140px;
 		}
 		.svg-stuff {
 			position: fixed;
-			bottom: calc(30% - 150px);
+			bottom: calc(24% - 150px);
 			width: 580px;
 			left: 50%;
 			margin-left: -290px;
@@ -58,6 +59,23 @@
 			position: relative;
 			z-index: 300;
 		}
+		.sticky-copyright {
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			padding: 5px 0;
+			margin: 0;
+			background-color: rgba(0,0,0,0.1);
+		}
+		.sticky-copyright * {
+			margin: 0;
+		}
+		.sticky-copyright, .sticky-copyright a {
+			color: #fff;
+		}
+		.sticky-copyright a {
+			text-decoration: underline;
+		}
 	</style>
 </head>
 <body>
@@ -67,8 +85,33 @@
 		<img class="svg-shelf-l" src="{{ url('assets/shelf_left.svg') }}" alt="">
 		<img class="svg-stuff" src="{{ url('assets/main_vector.svg') }}" width="480" alt="">
 	</div>
-	<div class="page-content container-fluid">
-		<input type="text" name="" id="">
+	<div class="page-content container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h1 class="text-center">თსუ - გამოცდის ადგილები</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 col-sm-offset-2">
+				<input type="text" class="form-control input-lg" placeholder="სახელი">
+			</div>
+			<div class="col-sm-5">
+				<input type="text" class="form-control input-lg" placeholder="გვარი">
+			</div>
+		</div>
+	</div>
+	<div class="sticky-copyright containter-fluid">
+		<div class="row">
+			<div class="text-center visible-xs col-xs-12">
+				Created with &hearts; by <a href="https://stichoza.com">Stichoza</a>, School vector designed by <a href="http://www.freepik.com/free-photos-vectors/school">Freepik</a>
+			</div>
+			<div class="hidden-xs text-left col-sm-6">
+				Created with &hearts; by <a href="https://stichoza.com">Stichoza</a>
+			</div>
+			<div class="hidden-xs text-right col-sm-6">
+				School vector designed by <a href="http://www.freepik.com/free-photos-vectors/school">Freepik</a>
+			</div>
+		</div>
 	</div>
 	<script src="{{ url('lib/jquery/dist/jquery.min.js') }}"></script>
 	<script src="{{ url('lib/bootstrap/dist/js/bootstrap.min.js') }}"></script>

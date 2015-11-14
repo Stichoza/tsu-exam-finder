f18bd89781e2abb4234f66cef8fb19c18b33632e
Levan Velijanashvili <stichoza@gmail.com>
2015-11-14 06:09:15 +0400

    Front-end stuff

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,13 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
+    private $localizedMonths = [
+        'ნულანვარი', 'იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი',
+        'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქრომბერი', 'ნოემბერი', 'დეკემბერი'
+    ];
 
     public function index()
     {
-        return view('index');
+        $dates = [];
+        $hours = [];
+        $carbon = new Carbon('yesterday'); // Because we add a day
+
+        for ($i = 0; $i < 2; $i++) {
+            $dates[] = [
+                'value' => $carbon->addDay()->format('j F Y'),
+                'title' => $carbon->day . ' ' . $this->localizedMonths[$carbon->month],
+                'active' => false // We will set it later
+            ];
+        }
+
+        $dates[(int) ($carbon->hour > 20)]['active'] = true; // Make one date selected depending on current time.
+
+        $carbon->setTime(8, 30); // Set time 30 minutes before first time.
+
+        for ($i = 0; $i < 18; $i++) {
+            $hours[] = $carbon->addMinutes(30)->format('H:i');
+        }
+
+        return view('index')->with('dates', $dates);
     }
 }

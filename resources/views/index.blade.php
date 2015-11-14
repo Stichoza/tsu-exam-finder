d7b5b0de4f49de90ca27b1984436378898541dca
Levan Velijanashvili <stichoza@gmail.com>
2015-11-14 18:41:02 +0400

    Huge refactor for JS/CSS -> Coffee/Stylus

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,96 +6,7 @@
 	<link rel="stylesheet" href="{{ url('lib/bootstrap/dist/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ url('lib/alk-life/css/alk-life.min.css') }}">
 	<link rel="stylesheet" href="{{ url('lib/sweetalert/dist/sweetalert.css') }}">
-	<style>
-		body {
-			background-color: #5ca0a0;
-			font-family: 'ALK Life';
-		}
-		.svg-desk {
-			position: fixed;
-			top: 0;
-			left: 0;
-			right: 0;
-			bottom: 24%;
-			background-image: url('assets/desk.svg');
-			background-size: cover;
-			z-index: 200;
-			box-shadow: 0 0 64px rgba(0,0,0,0.15);
-		}
-		.svg-shelf-r, .svg-shelf-l {
-			position: fixed;
-			z-index: 150;
-			left: 50%;
-			-webkit-transition: all .25s ease-in-out;
-			   -moz-transition: all .25s ease-in-out;
-			    -ms-transition: all .25s ease-in-out;
-			     -o-transition: all .25s ease-in-out;
-			        transition: all .25s ease-in-out;
-		}
-		.svg-shelf-r:hover, .svg-shelf-l:hover {
-			margin-bottom: 0;
-		}
-		.svg-shelf-r {
-			bottom: calc(24% - 52px);
-			margin-left: 240px;
-			margin-bottom: 20px;
-			width: 140px;
-		}
-		.svg-shelf-l {
-			bottom: calc(24% - 84px);
-			margin-left: -350px;
-			margin-bottom: 50px;
-			width: 140px;
-		}
-		.svg-stuff {
-			position: fixed;
-			bottom: calc(24% - 150px);
-			width: 580px;
-			left: 50%;
-			margin-left: -290px;
-			z-index: 250;
-			pointer-events: none;
-		}
-		.page-content {
-			position: relative;
-			z-index: 300;
-		}
-		.date, .time {
-			font-size: 28px;
-		}
-		.date > span::after {
-			display: inline;
-			content: ',';
-		}
-		.ui-clickable {
-			cursor: pointer;
-			border-bottom: 2px dotted transparent; 
-		}
-		.ui-clickable:hover {
-			border-bottom-color: black;
-		}
-		.sticky-copyright {
-			position: absolute;
-			bottom: 0;
-			width: 100%;
-			padding: 5px 0;
-			margin: 0;
-			background-color: rgba(0,0,0,0.1);
-		}
-		.sticky-copyright:hover {
-			z-index: 350;
-			background-color: rgba(0,0,0,0.3);
-		}
-		.sticky-copyright * {
-			margin: 0;
-		}
-		.sticky-copyright, .sticky-copyright a {
-			color: #fff;
-		}
-		.sticky-copyright a {
-			text-decoration: underline;
-		}
-	</style>
+	<link rel="stylesheet" href="{{ url('css/dist/main.min.css') }}">
 </head>
 <body>
 	<div class="svg-assets">
@@ -110,7 +21,7 @@
 				<h1 class="text-center">გამოცდის ადგილები (თსუ)</h1>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row sweet-inputs">
 			<div class="col-sm-3 col-sm-offset-2">
 				<input type="text" class="form-control input-lg" placeholder="სახელი">
 			</div>
